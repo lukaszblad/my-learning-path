8805a170d30f899e7c12b1a41f6c2f372c63c53e
lukaszblad <lukasz.blad.97@gmail.com>
2024-12-30 10:57:29 +0100

    Pro C# 10 setup solution for chapter 16

--- books/c#/pro-C#-10-with -.NET-6-2022/chapter-15/background-threads/obj/Debug/net8.0/background-threads.AssemblyInfo.cs
+++ books/c#/pro-C#-10-with -.NET-6-2022/chapter-15/background-threads/obj/Debug/net8.0/background-threads.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("background-threads")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c0d0aa2ffa237ea9e380aa1d65074010a95ee90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80bf46094ab96243b1eee5714d04b81096fdb311")]
 [assembly: System.Reflection.AssemblyProductAttribute("background-threads")]
 [assembly: System.Reflection.AssemblyTitleAttribute("background-threads")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
