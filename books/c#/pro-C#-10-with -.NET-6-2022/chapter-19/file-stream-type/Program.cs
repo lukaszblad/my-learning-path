fbb19b639fb2576f0f91ee3ac18405ad57572afe
lukaszblad <lukasz.blad.97@gmail.com>
2025-01-05 15:36:43 +0100

    Pro C# 10 chapter 19

---
+++ books/c#/pro-C#-10-with -.NET-6-2022/chapter-19/file-stream-type/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+using(FileStream fStream = File.Open("MyMessage.dat", FileMode.Create))
+{
+    string? msg = "Hello";
+    byte[] msgAsByteArray = Encoding.Default.GetBytes(msg);
+    fStream.Write(msgAsByteArray, 0, msgAsByteArray.Length);
+    fStream.Position = 0;
+
+    Console.Write("Your message as an array of bytes: ");
+    byte[] bytesFromFile = new byte[msgAsByteArray.Length];
+    for (int i = 0; i < msgAsByteArray.Length; i++)
+    {
+    bytesFromFile[i] = (byte)fStream.ReadByte();
+    Console.Write(bytesFromFile[i]);
+    }
+
+    Console.Write("\nDecoded Message: ");
+    Console.WriteLine(Encoding.Default.GetString(bytesFromFile));
+}
+File.Delete("MyMessage.dat");
